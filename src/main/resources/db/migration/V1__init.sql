1f4af76d1c4dcaa7c3234a9e251429c5f3cd7b19
gukov <m.gukov@cft.ru>
2021-03-19 22:50:02 +0700

    Поправил локальную версию

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,10 +1,11 @@
 SET NAMES utf8;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `secret` varchar(255) DEFAULT NULL,
-  `role` varchar(255) DEFAULT 'USER',
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS "users";
+
+CREATE TABLE "users" (
+  "id" bigint(20) NOT NULL,
+  "name" varchar(255) DEFAULT NULL,
+  "secret" varchar(255) DEFAULT NULL,
+  "role" varchar(255) DEFAULT 'USER',
+  PRIMARY KEY ("id")
 );
